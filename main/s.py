7fff679dcf97309cc634d52bb2aaac8a5d5983b5
Миша <yerafeyeum@gmail.com>
2025-12-05 18:04:50 +0300

    0.111

--- main/s.py
+++ main/s.py
@@ -1,3 +1,3 @@
-token = "8282811679:AAEiyy1FBMpTu-eOnBSsSddYM2VQnvSv0Fg" # основной
+# token = "8282811679:AAEiyy1FBMpTu-eOnBSsSddYM2VQnvSv0Fg" # основной
 
-# token = "8369459856:AAHDdO4DjtErKQppxxDYugMxrkFOkrewP2o" # тестовый
+token = "8369459856:AAHDdO4DjtErKQppxxDYugMxrkFOkrewP2o" # тестовый
