4c5aa7f49e71a55e8f3408668a9074f5ab498e19
Миша <yerafeyeum@gmail.com>
2025-12-08 17:55:07 +0300

    0.12

--- main/main.py
+++ main/main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import telebot
+from telebot import types
+from s import token
+
+bot = telebot.TeleBot(token)
+# Делаем херню для выбора мест монжо матрицей
+# Людей храним в дб? или в json
+# Соответственно поиск и запись человека 
+
+@bot.message_handler(commands = ['start'])
+def start_message(message):
+    user_id = message.from_user.id
+    bot.send_game(user_id, "DX_tickets")
+
+@bot.callback_query_handler(func = lambda callback_query: callback_query.game_short_name == "DX_tickets")
+def game(call):
+    bot.answer_callback_query(callback_query_id=call.id, url = "https://agar.io/")
+
+bot.polling(none_stop=True, interval = 0)
