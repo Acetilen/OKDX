d46cadce931ed82d4dd982a081434ab80be9f909
Миша <yerafeyeum@gmail.com>
2025-12-08 21:25:20 +0300

    0.14

--- main/main.py
+++ main/main.py
@@ -1,24 +1,46 @@
 import telebot ############!!!!!!!!!!!Сюда же пилим для баллов на конкурсах
 from telebot import types
+import time
+import re
 from s import token
+import sqlite3
+
+def escape_markdown(text): # экранирует markdown
+    escape_chars = r'[]()~`>#+-=|{}.!'
+    return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
 
 bot = telebot.TeleBot(token)
-# Делаем херню для выбора мест монжо матрицей
-# Людей храним в дб? или в json
-# Соответственно поиск и запись человека 
 
-@bot.message_handler(commands = ['start','go'])
-def start_play_message(message):
-    user_id = message.from_user.id
-    if message.text == "/start":
-        bot.send_message(user_id, "           Привет!\nЯ бот ОКДХ БГУ, через меня можно забронировать билеты на концерт, с помощью команды /go")
-    elif message.text == "/go":
-        bot.send_game(user_id, "DX_tickets")
-    else:
-        bot.send_message(user_id, "Hello World!")
+users = {}
 
-@bot.callback_query_handler(func = lambda callback_query: callback_query.game_short_name == "DX_tickets")
-def game(call):
-    bot.answer_callback_query(callback_query_id=call.id, url = "https://agar.io/")
+def botik():
+    try:
+        @bot.message_handler(commands = ['start','go'])
+        def start_go_message(message):
+            global users
+            user_id = message.from_user.id
+            if user_id not in list(users.keys()):
+                users.update({str(user_id):()})
+                print(users)
+            match message.text:
+                case "/start":
+                    bot.send_message(user_id, escape_markdown("*Привет!*👋\nЯ бот ОКДХ БГУ, через меня можно забронировать билеты на концерт, с помощью команды */go*"), parse_mode="markdownv2")
+                case "/go":
+                    bot.send_game(user_id, "DX_tickets")
+                case "/help":
+                    bot.send_message(user_id, escape_markdown("Лови список тех команды которые я знаю:\n */go* - я помогу тебе забронировать билеты на концерт\n */help* - ты получишь это сообщение"), parse_mode="markdownv2")
+                case _:
+                    bot.send_message(user_id, escape_markdown("Такой команды у меня пока нет\n Вот список команд которые мне знакомы:\n */go* - я помогу тебе забронировать билеты на концерт\n */help* - ты получишь это сообщение"), parse_mode="markdownv2")
 
-bot.polling(none_stop=True, interval = 0)
+        @bot.callback_query_handler(func = lambda callback_query: callback_query.game_short_name == "DX_tickets")
+        def game(call):
+            bot.answer_callback_query(callback_query_id=call.id, url = "https://onliner.by/")
+
+
+        bot.polling(none_stop=True, interval = 0)
+    except:
+        print("Бот умер!")
+        time.sleep(1)
+        botik()
+
+botik()
--- main/s.py
+++ main/s.py
@@ -1,1 +1,16 @@
 token = "8369459856:AAHDdO4DjtErKQppxxDYugMxrkFOkrewP2o" # тестовый
+
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+               id INTEGER PRIMARY KEY
+               )
+               ''')
+connection.close()
+
+
+
+
